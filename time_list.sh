701a81c3be4e443e3a810e657618e1063cecf402
QianZhuJLU <49641288+QianZhuJLU@users.noreply.github.com>
2020-07-13 10:12:42 +0800

    Update time_list.sh

--- time_list.sh
+++ time_list.sh
@@ -1,18 +1,18 @@
-mulu=`ls -d */`
-for i in $mulu ;do
+directory=`ls -d */`
+for i in $directory ;do
     touch time_used
     cd $i
     echo "-----------------" >> ../time_used
     pwd|awk 'BEGIN{FS="/"}{print $5}' >> ../time_used
     if [ -f "OUTCAR" ];then
-        uuu=`grep time OUTCAR |tail -1|awk '{print $1}'`
-        if  [ "$uuu" == "Elapsed" ];then
+        used=`grep time OUTCAR |tail -1|awk '{print $1}'`
+        if  [ "$used" == "Elapsed" ];then
           ~/vtime.sh >> ../time_used
         else
-          echo "oh,NO,did not get COVERAGED" >> ../time_used
+          echo "oh NO,did not get COVERAGED" >> ../time_used
         fi
     else 
-      echo "NO OUTCAR" >> ../time_used
+      echo "NO OUTCAR !" >> ../time_used
     fi
     cd ..
 done 
